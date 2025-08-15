49df6a567b9c3b2d3e2026676b5e37c5a72df83c
Saiem Saeed <saiem.saeed7@gmail.com>
2025-08-15 23:26:40 +0200

    Lexer now support Line Number and Column Number

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -8,11 +8,13 @@ type Lexer struct {
 	input        string
 	position     int
 	readPosition int
+	currLine     int
+	currColumn   int
 	ch           byte
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
+	l := &Lexer{input: input, currLine: 1, currColumn: 1}
 	l.readChar()
 	return l
 }
@@ -27,42 +29,41 @@ func (l *Lexer) NextToken() token.Token {
 		if l.peekChar() == '=' {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Literal: string(ch) + string(l.ch), Type: token.EQ}
+			tok = newToken(token.EQ, string(ch)+string(l.ch), l.currLine, l.currColumn)
 		} else {
-			tok = newToken(token.ASSIGN, l.ch)
+			tok = newToken(token.ASSIGN, string(l.ch), l.currLine, l.currColumn)
 		}
 	case '+':
-		tok = newToken(token.PLUS, l.ch)
+		tok = newToken(token.PLUS, string(l.ch), l.currLine, l.currColumn)
 	case '-':
-		tok = newToken(token.MINUS, l.ch)
+		tok = newToken(token.MINUS, string(l.ch), l.currLine, l.currColumn)
 	case '*':
-		tok = newToken(token.ASTERISK, l.ch)
+		tok = newToken(token.ASTERISK, string(l.ch), l.currLine, l.currColumn)
 	case '/':
-		tok = newToken(token.SLASH, l.ch)
+		tok = newToken(token.SLASH, string(l.ch), l.currLine, l.currColumn)
 	case ',':
-		tok = newToken(token.COMMA, l.ch)
+		tok = newToken(token.COMMA, string(l.ch), l.currLine, l.currColumn)
 	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
+		tok = newToken(token.SEMICOLON, string(l.ch), l.currLine, l.currColumn)
 	case '(':
-		tok = newToken(token.LPAREN, l.ch)
+		tok = newToken(token.LPAREN, string(l.ch), l.currLine, l.currColumn)
 	case ')':
-		tok = newToken(token.RPAREN, l.ch)
+		tok = newToken(token.RPAREN, string(l.ch), l.currLine, l.currColumn)
 	case '{':
-		tok = newToken(token.LBRACE, l.ch)
+		tok = newToken(token.LBRACE, string(l.ch), l.currLine, l.currColumn)
 	case '<':
-		tok = newToken(token.LT, l.ch)
+		tok = newToken(token.LT, string(l.ch), l.currLine, l.currColumn)
 	case '>':
-		tok = newToken(token.GT, l.ch)
+		tok = newToken(token.GT, string(l.ch), l.currLine, l.currColumn)
 	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-
+		tok = newToken(token.RBRACE, string(l.ch), l.currLine, l.currColumn)
 	case '!':
 		if l.peekChar() == '=' {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+			tok = newToken(token.NOT_EQ, string(ch)+string(l.ch), l.currLine, l.currColumn)
 		} else {
-			tok = newToken(token.BANG, l.ch)
+			tok = newToken(token.BANG, string(l.ch), l.currLine, l.currColumn)
 		}
 	case 0:
 		tok.Literal = ""
@@ -71,17 +72,20 @@ func (l *Lexer) NextToken() token.Token {
 		if isDigit(l.ch) {
 			tok.Type = token.INT
 			tok.Literal = l.readNumber()
+			tok.Line = l.currLine
+			tok.Column = l.currColumn - len(tok.Literal) - 1 // Reading one extra characters in readNumber
 			return tok
 		} else if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
+			tok.Line = l.currLine
+			tok.Column = l.currColumn - len(tok.Literal) - 1 // Reading one extra character in readIdentifier
 			return tok
 		} else {
 			tok.Literal = ""
 			tok.Type = token.ILLEGAL
 		}
 	}
-
 	l.readChar()
 	return tok
 }
@@ -92,8 +96,9 @@ func (l *Lexer) readChar() {
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
-	l.position = l.readPosition
 
+	l.currColumn = l.currColumn + 1
+	l.position = l.readPosition
 	l.readPosition = l.readPosition + 1
 }
 
@@ -107,6 +112,11 @@ func (l *Lexer) peekChar() byte {
 
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\n' || l.ch == '\t' || l.ch == '\r' {
+		if l.ch == '\n' {
+			l.currLine++
+			l.currColumn = 0
+		}
+
 		l.readChar()
 	}
 }
@@ -143,9 +153,11 @@ func isDigit(ch byte) bool {
 	return false
 }
 
-func newToken(tokenType token.TokenType, ch byte) token.Token {
+func newToken(tokenType token.TokenType, ch string, line int, column int) token.Token {
 	return token.Token{
 		Type:    tokenType,
 		Literal: string(ch),
+		Line:    line,
+		Column:  column - len(string(ch)),
 	}
 }
